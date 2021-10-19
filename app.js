a81566cf39d3c04410556cbd504ebcecbd3c1180
Biraj <birajparajuli2001@gmail.com>
2021-10-19 21:11:18 +0545

    working on api

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello from the other side");
+const treks = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/treks-data.json`)
+);
+
+app.get("/api/v1/treks", (req, res) => {
+  res.json({
+    status: "success",
+    results: treks.length,
+    data: {
+      treks,
+    },
+  });
+});
+
+app.post("/api/v1/treks", (req, res) => {
+  const newId = treks[treks.length - 1].id + 1;
+  const newTrek = Object.assign({ id: newId }, req.body);
+  treks.push(newTrek);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/treks-data.json`,
+    JSON.stringify(treks),
+    (err) => {
+      console.log(err);
+      res.json({
+        status: "success",
+        data: {
+          trek: newTrek,
+        },
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
