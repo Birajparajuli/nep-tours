c72bbd80ba4f338bddd38a0fb9c3dfae98bc0d2e
Biraj <birajparajuli2001@gmail.com>
2021-10-22 20:44:45 +0545

    connected to local mongo db

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const mongoose = require("mongoose");
+
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log("Database Connection Successful!");
+  });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
