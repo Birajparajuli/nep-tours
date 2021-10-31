47b73973d1f70d338f296905741b373e6e3ddb28
Biraj <birajparajuli2001@gmail.com>
2021-10-31 19:41:11 +0545

    code refactoring

--- controllers/trekController.js
+++ controllers/trekController.js
@@ -1,4 +1,5 @@
 const Trek = require("./../models/trekModel");
+const APIFeatures = require("./../utils/apiFeature");
 
 exports.aliasTopTreks = (req, res, next) => {
   req.query.limit = "5";
@@ -12,53 +13,58 @@ exports.aliasLatestTrek = (req, res, next) => {
   req.query.sort = "-createdAt";
   next();
 };
+
 exports.getAllTreks = async (req, res) => {
   try {
-    console.log(req.query);
+    // console.log(req.query);
 
-    //1. Filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((el) => delete queryObj[el]);
+    // //1. Filtering
+    // const queryObj = { ...req.query };
+    // const excludedFields = ["page", "sort", "limit", "fields"];
+    // excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 2. Advanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    // console.log(JSON.parse(queryStr));
+    // // 2. Advanced Filtering
+    // let queryStr = JSON.stringify(queryObj);
+    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // // console.log(JSON.parse(queryStr));
 
-    let query = Trek.find(JSON.parse(queryStr));
+    // let query = Trek.find(JSON.parse(queryStr));
 
-    //3. Sorting
+    // //3. Sorting
 
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("-createdAt");
-    }
+    // if (req.query.sort) {
+    //   const sortBy = req.query.sort.split(",").join(" ");
+    //   query = query.sort(sortBy);
+    // } else {
+    //   query = query.sort("-createdAt");
+    // }
 
-    // 4. Field Limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v");
-    }
+    // // 4. Field Limiting
+    // if (req.query.fields) {
+    //   const fields = req.query.fields.split(",").join(" ");
+    //   query = query.select(fields);
+    // } else {
+    //   query = query.select("-__v");
+    // }
 
-    // 5. Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
+    // // 5. Pagination
+    // const page = req.query.page * 1 || 1;
+    // const limit = req.query.limit * 1 || 100;
+    // const skip = (page - 1) * limit;
 
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTrek = await Tour.countDocuments();
-      if (skip >= numTrek) throw new Error("This page dose nor exit !!!");
-    }
+    // if (req.query.page) {
+    //   const numTrek = await Tour.countDocuments();
+    //   if (skip >= numTrek) throw new Error("This page dose nor exit !!!");
+    // }
 
     // Execute Query
-    const treks = await query;
+    const features = new APIFeatures(Trek.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const treks = await features.query;
     //query.sort().select().skip().limit()
 
     // Send Response
